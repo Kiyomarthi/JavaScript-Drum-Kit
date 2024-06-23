3eba8317d965df48c34e25d5d96b901b7adcdcf7
Kiyomarthi <a.kiyomarthi@gmail.com>
2024-06-23 10:46:33 +0330

    feat: add the handlePlayMusic funciton

--- logic.js
+++ logic.js
@@ -1,14 +1,26 @@
 const audios = document.querySelectorAll("#keyAudio");
 const keys = document.querySelectorAll(".key");
-let arr = [];
-keys.forEach((key) => (arr = [...arr, key.getElementsByTagName("kbd")]));
+// let arr = [];
+// keys.forEach((key) => (arr = [...arr, key.getElementsByTagName("kbd")]));
+// keys.forEach((i) => console.log(i));
 
-function handlePlayMusic(e) {
-  arr.forEach((i) => {
-    if (i[0].innerHTML.toLowerCase() === e.key.toLowerCase()) {
-      console.log("ok");
+function handleGetKey(e) {
+  keys.forEach((key) => {
+    if (
+      key.getElementsByTagName("kbd")[0].innerHTML.toLowerCase() ===
+      e.key.toLowerCase()
+    ) {
+      handlePlayMusic(key.getAttribute("data-key"));
     }
   });
 }
 
-window.addEventListener("keydown", (e) => handlePlayMusic(e));
+function handlePlayMusic(dataKey) {
+  audios.forEach((audio) => {
+    if (audio.getAttribute("data-key") === dataKey) {
+      audio.play();
+    }
+  });
+}
+
+window.addEventListener("keydown", (e) => handleGetKey(e));
